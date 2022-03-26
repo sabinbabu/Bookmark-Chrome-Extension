6096866634efcb859a0e850f7acede7ddfc9c8cc
Sabin Acharya <sabinacharya444@gmail.com>
2022-03-26 20:35:44 +1100

    templete string added

--- index.js
+++ index.js
@@ -2,18 +2,24 @@ let myBookmarks = []
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
+const errorEL = document.getElementById("error-el")
 
-inputBtn.addEventListener("click", function(){   
-    myBookmarks.push(inputEl.value)
-    showBookmarks()   
+inputBtn.addEventListener("click", function(){  
+        const inputValue  = inputEl.value
+        myBookmarks.push(inputValue)
+        inputEl.value = "";
+        showBookmarks()       
+      
 });
 
 function showBookmarks(){
     let listItems = ""
     for(item of myBookmarks){
-        listItems += "<li>" + item + "</li> "
+        listItems += `
+        <li> 
+            <a target = _blank href= https://${item}> ${item} </a>
+        </li> `
     }
-    
     ulEl.innerHTML = listItems
     
 }
