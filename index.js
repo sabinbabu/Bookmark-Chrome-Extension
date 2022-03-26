fcf53908772aa3fad10129e9b0c8163bc9f8b2ff
Sabin Acharya <sabinacharya444@gmail.com>
2022-03-26 15:16:47 +1100

    bookmark adding functionality

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-let inputBtnEl = document.getElementById("input-btn")
-inputBtnEl.addEventListener("click", function(){
-    console.log("button clicked")
+let myBookmarks = []
+const inputBtn = document.getElementById("input-btn")
+const inputEl = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-el")
+
+inputBtn.addEventListener("click", function(){   
+    myBookmarks.push(inputEl.value)
+    showBookmarks()   
 });
+
+function showBookmarks(){
+    let listItems = ""
+    for(item of myBookmarks){
+        listItems += "<li>" + item + "</li> "
+    }
+    
+    ulEl.innerHTML = listItems
+    
+}
